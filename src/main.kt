1c2f05ba02955aaadbfbcf518f0b1a76882f670b
joycenjichile <joycenkimalantoi@gmail.com>
2022-03-10 08:39:14 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,30 @@
+fun main(){
+    String()
+
+}
+fun String(){
+    var index="codeHive"
+    print(index[4])
+    print(index[5])
+    print(index[6])
+    println(index[7])
+   var y= reminder(8,3)
+    println(y)
+    var j=parameters("joyce",26)
+    println(j)
+
+}
+fun reminder(a:Int,b:Int):Int{
+    var x=8%3
+    return x
+
+
+}
+fun parameters(d:String,e:Int){
+    var q=("Hi,my name is $d,and iam $e years old")
+    println(q)
+
+
+
+
+}
